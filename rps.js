fe07e4534a24e474c489ea9ab5f26eb3afe234d8
Sergio Bermejo <sergio.bermejo123@gmail.com>
2022-05-02 21:50:56 +0200

    Desarrollo final
    Para añadir complejidad y opciones hacer nueva iteración. Esta arrastra muchos problemas aunque ha sido bastante interesante. TO-DO: trabajar el output al html y el problema de los empates, principalmente.

--- rps.js
+++ rps.js
@@ -1,11 +1,34 @@
+let finalscore
+let compPick
+let userPick
+let results 
+let round
+
 function GameRPS(){
-    let compPick
-    let userPick
-    userSelection();
-    computerSelection();
-    //Empates
-    let results 
-    getResults();
+finalscore=0
+    //Bucle y contador de rondas. El límite de rondas es 5. Los empates cuentan como ronda.
+    for(round = 1; round <6; round++){
+        userSelection();
+        computerSelection();
+        getResults();
+    //si alguien llega a 3 puntos antes de las 5 rondas, la partida termina.
+        if(finalscore == -3|| finalscore == +3) {
+            break;
+        }
+    }
+
+    //BO5: el primero en ganar 3 gana || casos: 2-3 3-2 || -1 ó +1 
+    //si el resultado es positivo, gana el player, si no, gana el ordenador.
+
+    if(finalscore > 0) {
+        document.body.append('Tú ganas la partida!')
+        }
+    else if (finalscore < 0) {
+        document.body.append('Tú pierdes la partida!')
+        }
+    else {
+        document.body.append('Vaya, habéis empatado!')
+        }
     }
 
 function computerSelection(){
@@ -32,35 +55,42 @@ function userSelection(){
 
 function getResults(){
     results = compPick+userPick
+    //esta es la verdadera chapuza de la iteración
     switch (results) {
         case 'tijeratijera':
-            document.body.append('Empate! ');
+            document.body.append('\nResultado de la ronda '+round+': empate! ');
             break;
         case 'piedrapiedra':
-            document.body.append('Empate! ');
+            document.body.append('\n Resultado de la ronda '+round+': empate! ');
             break;
         case 'papelpapel':
-            document.body.append('Empate! ');
+            document.body.append('\n Resultado de la ronda '+round+': empate! ');
             break;
 
         case 'piedratijera':
-            document.body.append('Tú pierdes! ');
+            document.body.append('\n Resultado de la ronda '+round+': Tú pierdes! ');
+            finalscore = finalscore - 1
             break;
         case 'tijerapapel':
-            document.body.append('Tú pierdes! ');
+            document.body.append('\n Resultado de la ronda '+round+': Tú pierdes! ');
+            finalscore = finalscore - 1
             break;
         case 'papelpiedra':
-            document.body.append('Tú pierdes! ');
+            document.body.append('\nResultado de la ronda '+round+': Tú pierdes! ');
+            finalscore = finalscore - 1
             break;
 
         case 'tijerapiedra':
-            document.body.append('Tú ganas! ');
+            document.body.append('\nResultado de la ronda '+round+': Tú ganas! ');
+            finalscore = finalscore + 1
             break;
         case 'papeltijera':
-            document.body.append('Tú ganas! ');
+            document.body.append('\nResultado de la ronda '+round+': Tú ganas! ');
+            finalscore = finalscore + 1
             break;
         case 'piedrapapel':
-            document.body.append('Tú ganas! ');
+            document.body.append('\nResultado de la ronda '+round+': Tú ganas! ');
+            finalscore = finalscore + 1
             break;
     }
 }
