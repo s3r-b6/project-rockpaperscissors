82505b92368153cf6484528eaea9c2f6338befc3
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-05-06 21:48:36 +0200

    Código casi funcional
    Falta añadir el marcador, que sea un BO5 etc., pero lo más duro está hecho.

--- rps.js
+++ rps.js
@@ -2,49 +2,59 @@
 // 2>1 => gana 1 (piedra)
 // 3>2 => gana 2 (tijera)
 // 1>3 ?????
-// aritmética modular (¿?) usando % se reducen los casos
-
-//UserPick
-let userChoice;
-
-let CompChoice;
-
-function computerPick() {
-  let random = Math.floor(Math.random() * 10);
-  console.log(random);
-  if (random <= 3) {
-    CompChoice = 0;
-  } else if (random > 3 && random <= 6) {
-    CompChoice = 1;
-  } else if (random > 6 && random <= 10) {
-    CompChoice = 2;
+function PlayRPS(){
+  let CompChoice;
+  function computerPick() {
+    let random = Math.floor(Math.random() * 10);
+    console.log(random);
+    if (random <= 3) {
+      CompChoice = 0;
+    } else if (random > 3 && random <= 6) {
+      CompChoice = 1;
+    } else if (random > 6 && random <= 10) {
+      CompChoice = 2;
+    }
   }
-}
 
-computerPick();
+  /*
+    tests
+    console.log(CompChoice)
+    userChoice = 2
 
-/* tests
-console.log(CompChoice)
-userChoice = 0
+    console.log((userChoice+1)%3)
+    console.log(CompChoice % 3)
+  */
+  /*
+    no entiendo bien el concepto, pero funciona; esta es la demostración más fácil pero  (https://math.stackexchange.com/q/391185):
+    % 3, option k beats option k+1 and is beaten by option k−1
 
-console.log((userChoice+1)%3)
-console.log(CompChoice % 3)
+    muy interesante: https://en.wikipedia.org/wiki/Modular_arithmetic
+  */
 
-*/
-
-/*
-  no entiendo el concepto, pero funciona; esta es una explicación más fácil pero jamás se me habría ocurrido (https://math.stackexchange.com/q/391185):
-  % 3, option k beats option k+1 and is beaten by option k−1
-*/
-
-function GameRPS() {
-  if (userChoice == CompChoice) {
-    console.log("Empate!");
-  } else if ((userChoice+1)%3 == CompChoice % 3) {
-    console.log("Tú ganas!");
-  } else{
-    console.log("Gana el PC!");
+  function GameRPS(userChoice) {
+    computerPick();
+    if (userChoice == CompChoice) {
+      document.body.append("Empate!");
+    } else if ((userChoice + 1) % 3 == CompChoice % 3) {
+      document.body.append("Gana el PC!");
+    } else {
+      document.body.append("Tú ganas!");
+    }
   }
-}
 
-GameRPS()
+  const piedra = document.getElementById("piedra-b");
+  const papel = document.getElementById("papel-b");
+  const tijera = document.getElementById("tijera-b");
+
+//(https://stackoverflow.com/a/24050476) Para pasarle un argumento al eventhandler inline hay que usar una función anónima
+//Tb esta es la razón por la que el addEventListener no estaba funcionando 
+
+  piedra.onclick = function () {GameRPS(0);};
+  papel.onclick = function () {GameRPS(1);};
+  tijera.onclick = function () {GameRPS(2);};
+  
+// Tb se puede usar el bind (pasa a la función un array con parámetros):
+//tijera.onclick = GameRPS.bind(this, [2]);
+
+}
+window.onload = PlayRPS;
