7cc18d32466c41c17b8a3be3b757083bb19a07c5
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-05-06 10:31:07 +0200

    Primer commit
    Esbozo de la GUI, código prácticamente sin empezar

--- rps.js
+++ rps.js
@@ -1,99 +1,20 @@
-let finalscore
-let compPick
-let userPick
-let results 
-let round
+document.getElementById("RPS-Button").addEventListener("click", GameRPS());
 
-function GameRPS(){
-finalscore=0
-    //Bucle y contador de rondas. El límite de rondas es 5. Los empates cuentan como ronda.
-    for(round = 1; round <6; round++){
-        userSelection();
-        computerSelection();
-        getResults();
-    //si alguien llega a 3 puntos antes de las 5 rondas, la partida termina.
-        if(finalscore == -3|| finalscore == +3) {
-            break;
-        }
-    }
+function GameRPS() {
+  const app = document.querySelector("#app");
 
-    //BO5: el primero en ganar 3 gana || casos: 2-3 3-2 || -1 ó +1 
-    //si el resultado es positivo, gana el player, si no, gana el ordenador.
+  const button1 = document.createElement("button");
+  button1.classList.add("Botón", "Piedra");
+  button1.textContent = "Piedra";
+  app.appendChild(button1);
 
-    if(finalscore > 0) {
-        document.body.append('Tú ganas la partida!')
-        }
-    else if (finalscore < 0) {
-        document.body.append('Tú pierdes la partida!')
-        }
-    else {
-        document.body.append('Vaya, habéis empatado!')
-        }
-    }
+  const button2 = document.createElement("button");
+  button2.classList.add("Botón", "Papel");
+  button2.textContent = "Papel";
+  app.appendChild(button2);
 
-function computerSelection(){
-    let PickValue;
-    PickValue = Math.floor(Math.random() * 10)
-        if (PickValue >= 0 && PickValue < 3) {
-            compPick = 'piedra';
-        }
-        if (PickValue >= 3 && PickValue < 6) {
-            compPick = 'papel';
-        }
-        if (PickValue >= 6 && PickValue <= 9) {
-            compPick = 'tijera';
-    }
+  const button3 = document.createElement("button");
+  button3.classList.add("Botón", "Tijera");
+  button3.textContent = "Tijera";
+  app.appendChild(button3);
 }
-
-function userSelection(){
-    userPick = prompt('Piedra, papel o tijera?');
-    userPick = userPick.toLowerCase();
-    if(!userPick.match(/^(piedra|papel|tijera)$/)){
-        userSelection();
-    }
-}
-
-function getResults(){
-    results = compPick+userPick
-    //esta es la verdadera chapuza de la iteración
-    switch (results) {
-        case 'tijeratijera':
-            document.body.append('\nResultado de la ronda '+round+': empate! ');
-            break;
-        case 'piedrapiedra':
-            document.body.append('\n Resultado de la ronda '+round+': empate! ');
-            break;
-        case 'papelpapel':
-            document.body.append('\n Resultado de la ronda '+round+': empate! ');
-            break;
-
-        case 'piedratijera':
-            document.body.append('\n Resultado de la ronda '+round+': Tú pierdes! ');
-            finalscore = finalscore - 1
-            break;
-        case 'tijerapapel':
-            document.body.append('\n Resultado de la ronda '+round+': Tú pierdes! ');
-            finalscore = finalscore - 1
-            break;
-        case 'papelpiedra':
-            document.body.append('\nResultado de la ronda '+round+': Tú pierdes! ');
-            finalscore = finalscore - 1
-            break;
-
-        case 'tijerapiedra':
-            document.body.append('\nResultado de la ronda '+round+': Tú ganas! ');
-            finalscore = finalscore + 1
-            break;
-        case 'papeltijera':
-            document.body.append('\nResultado de la ronda '+round+': Tú ganas! ');
-            finalscore = finalscore + 1
-            break;
-        case 'piedrapapel':
-            document.body.append('\nResultado de la ronda '+round+': Tú ganas! ');
-            finalscore = finalscore + 1
-            break;
-    }
-}
-
-
-//moraleja: habría sido mejor trabajar con ints todo el rato en lugar de strings.
