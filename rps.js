2566200af56fa0ef22718b40cb4684b24037c001
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-05-08 22:18:15 +0200

    JS funcionando, añadidas animaciones
    Faltaría añadir el marcador y el contador de rondas, hacer que el botón aparezca al terminar la ronda y estilar

--- rps.js
+++ rps.js
@@ -5,13 +5,12 @@ function PlayRPS() {
   //función que genera aleatoriamente el pick del ordenador
   function computerPick() {
     let random = Math.floor(Math.random() * 10);
-    console.log(random);
     if (random <= 3) {
-      CompChoice = 0;
+      return 0;
     } else if (random > 3 && random <= 6) {
-      CompChoice = 1;
+      return 1;
     } else if (random > 6 && random <= 10) {
-      CompChoice = 2;
+      return 2;
     }
   }
 
@@ -22,11 +21,14 @@ function PlayRPS() {
     muy interesante: https://en.wikipedia.org/wiki/Modular_arithmetic
   */
 
-  function GameRPS(userChoice) {
-    computerPick();
-    if (userChoice == CompChoice) {
+  var UserChoice;
+  var CompChoice;
+
+  //función principal
+  function GameRPS() {
+    if (UserChoice == CompChoice) {
       document.body.append("Empate!");
-    } else if ((userChoice + 1) % 3 == CompChoice % 3) {
+    } else if ((UserChoice + 1) % 3 == CompChoice % 3) {
       document.body.append("Gana el PC!");
     } else {
       document.body.append("Tú ganas!");
@@ -48,29 +50,28 @@ function PlayRPS() {
     });
   }
 
+  //enseña los picks
   function showPicks() {
-    let rPlaceU = document.querySelector(".user-selection-r");
-    let pPlaceU = document.querySelector(".user-selection-p");
-    let sPlaceU = document.querySelector(".user-selection-s");
+    var rPlaceU = document.querySelector(".user-selection-r");
+    var pPlaceU = document.querySelector(".user-selection-p");
+    var sPlaceU = document.querySelector(".user-selection-s");
 
-    let rPlaceC = document.querySelector(".comp-selection-r");
-    let pPlaceC = document.querySelector(".comp-selection-p");
-    let sPlaceC = document.querySelector(".comp-selection-s");
+    var rPlaceC = document.querySelector(".comp-selection-r");
+    var pPlaceC = document.querySelector(".comp-selection-p");
+    var sPlaceC = document.querySelector(".comp-selection-s");
 
     //USER
     setTimeout(() => {
       makeinv(".button.user");
 
       //hace aparecer el pick
-      if ((userChoice = 0)) {
+      if (UserChoice == 0) {
         rPlaceU.classList.replace("f-inv", "f-vis");
-      } else if ((userChoice = 1)) {
+      } else if (UserChoice == 1) {
         pPlaceU.classList.replace("f-inv", "f-vis");
-      } else if ((userChoice = 2)) {
+      } else if (UserChoice == 2) {
         sPlaceU.classList.replace("f-inv", "f-vis");
       }
-
-      //
     }, 200);
 
     //PC
@@ -79,11 +80,11 @@ function PlayRPS() {
       makeinv(".button.pc");
 
       //hace aparecer el pick
-      if ((CompChoice = 0)) {
+      if (CompChoice == 0) {
         rPlaceC.classList.replace("f-inv", "f-vis");
-      } else if ((CompChoice = 1)) {
+      } else if (CompChoice == 1) {
         pPlaceC.classList.replace("f-inv", "f-vis");
-      } else if ((CompChoice = 2)) {
+      } else if (CompChoice == 2) {
         sPlaceC.classList.replace("f-inv", "f-vis");
       }
     }, 600);
@@ -98,16 +99,26 @@ function PlayRPS() {
   // Tb se puede usar el bind (pasa a la función un array con parámetros):
   //tijera.onclick = GameRPS.bind(this, [2]);
 
+  //log debug
   piedra.addEventListener("click", () => {
-    GameRPS(0);
+    UserChoice = 0;
+    CompChoice = computerPick();
+    console.log("us:" + UserChoice + " vs " + "pc:" + CompChoice);
+    GameRPS();
     showPicks();
   });
   papel.addEventListener("click", () => {
-    GameRPS(1);
+    UserChoice = 1;
+    CompChoice = computerPick();
+    console.log("us:" + UserChoice + " vs " + "pc:" + CompChoice);
+    GameRPS();
     showPicks();
   });
   tijera.addEventListener("click", () => {
-    GameRPS(2);
+    UserChoice = 2;
+    CompChoice = computerPick();
+    console.log("us:" + UserChoice + " vs " + "pc:" + CompChoice);
+    GameRPS();
     showPicks();
   });
 
